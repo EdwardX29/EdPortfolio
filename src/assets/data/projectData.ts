9015dbd682a291b4f79f1963a325f5292a2f5e67
Edward <edwardxiao29@gmail.com>
2022-08-15 13:01:55 -0400

    inital commit

---
+++ src/assets/data/projectData.ts
@@ -0,0 +1,93 @@
+const projects = [
+
+    {
+        name: 'MoCoCovid',
+        technologies: ['Python', 'PHP', 'MySQL', 'Matplotlib', 'SMTP', 'API' ],
+        description: 'prom app',
+        GithubLink: '',
+        DemoLink: 'https://mococovid.com',
+        ImageLink:'',
+    },
+
+    {
+        name: 'Musical Metamorphasis Research',
+        technologies: ['Python', 'Jupyter Notebook', 'Billboard API', 'Spotify API',
+                        'Genius API',  'Matplotlib', 'Scipy', 'statsmodels' ],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/MusicMeta_DataAnalysis',
+        DemoLink: 'https://drive.google.com/file/d/1O3GDfK4pVOHF6eLAF9Fg8hFujwIiqEBQ/view',
+        ImageLink:'',
+    },
+
+    {
+        name: 'UMDConfessions',
+        technologies: ['Django', 'Bootstrap5', 'PostgreSQL','CAS', 'CRUD'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/umd-confessions',
+        DemoLink: 'https://umd-confessions.herokuapp.com/',
+        ImageLink:'',
+    },
+    {
+        name: 'Spotify Higher or Lower',
+        technologies: ['React', 'Javascript/TypeScript', 'CSS', 'Vitejs'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
+        DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
+        ImageLink:'',
+    },
+    {
+        name: 'NWProm',
+        technologies: ['Python', 'Django', 'Bootstrap5', 'PostgreSQL', 'Google SSO', 
+                        'Javascript/Typescript'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/nwprom',
+        DemoLink: 'https://nwprom.herokuapp.com/',
+        ImageLink:'',
+
+    },
+
+    {
+        name: 'Bypass Google Docs',
+        technologies: ['Python', 'Flask', 'BeautifulSoup', 'Pandas', 'Bootstrap5', 'CSS'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
+        DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
+        ImageLink:'',
+    },
+
+    {
+        name: 'Biographify',
+        technologies: ['Python', 'Django', 'Bootstrap5', 'Spotify API', 
+                        'Spotify SSO', 'CSS', 'html2canvas'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/biographify',
+        DemoLink: 'https://biographify.herokuapp.com/',
+        ImageLink:'',
+    },
+
+    {
+        name: 'AP Award Maker',
+        technologies: ['Javascript/Typescript', 'React', 'Vitejs', 'CSS', 'Framer'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/ap-award-clone',
+        DemoLink: 'https://edwardx29.github.io/ap-award-clone/',
+        ImageLink:'',
+    },
+
+    {
+        name: 'Carti Clicker',
+        technologies: ['Javascript/Typescript', 'React', 'Vitejs', 'CSS'],
+        description: 'prom app',
+        GithubLink: 'https://github.com/EdwardX29/CartiClicker',
+        DemoLink: 'https://edwardx29.github.io/CartiClicker/',
+        ImageLink:'',
+    },
+
+
+
+
+
+]
+
+
+export default projects;
