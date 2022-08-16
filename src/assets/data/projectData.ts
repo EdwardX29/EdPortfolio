bd149aa4f2e69428c9861298828a1be79e90c441
Edward <edwardxiao29@gmail.com>
2022-08-16 10:16:18 -0400

    project page

--- src/assets/data/projectData.ts
+++ src/assets/data/projectData.ts
@@ -3,84 +3,87 @@ const projects = [
     {
         name: 'MoCoCovid',
         technologies: ['Python', 'PHP', 'MySQL', 'Matplotlib', 'SMTP', 'API' ],
-        description: 'prom app',
+        description: `A COVID-19 informational newsletter website for COVID-19 data in Montgomery County Maryland. Delievered clear, concise, data reports weekly. 
+                        Assembled automated school dashboard with Tableau. Served 300+ weekly newsletter subscribers over 1 year.`,
         GithubLink: '',
         DemoLink: 'https://mococovid.com',
-        ImageLink:'',
+        ImageLink:'src/assets/images/moco.png',
     },
 
     {
         name: 'Musical Metamorphasis Research',
-        technologies: ['Python', 'Jupyter Notebook', 'Billboard API', 'Spotify API',
-                        'Genius API',  'Matplotlib', 'Scipy', 'statsmodels' ],
-        description: 'prom app',
+        technologies: ['Python', 'Jupyter', 'API', 'Matplotlib', 'Scipy', 'statsmodels' ],
+        description: `The perfect-scoring, final Ulysses Research project exploring the evolution of popular 
+        music in the 2000s & 2010s through data science. Analysed ~9000 songs spanning two decades of American pop music.`,
         GithubLink: 'https://github.com/EdwardX29/MusicMeta_DataAnalysis',
         DemoLink: 'https://drive.google.com/file/d/1O3GDfK4pVOHF6eLAF9Fg8hFujwIiqEBQ/view',
-        ImageLink:'',
+        ImageLink:'src/assets/images/musicmeta.jpg',
     },
 
     {
         name: 'UMDConfessions',
         technologies: ['Django', 'Bootstrap5', 'PostgreSQL','CAS', 'CRUD'],
-        description: 'prom app',
+        description: `UMDConfessions is an anonymous confessions 
+        and forum website with CAS authentication for UMD students. 
+        Leverage full fledged CRUD functionality while expressing your (perhaps 
+        hyperbolic) confessions. `,
         GithubLink: 'https://github.com/EdwardX29/umd-confessions',
         DemoLink: 'https://umd-confessions.herokuapp.com/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/umdconfessions.png',
     },
     {
         name: 'Spotify Higher or Lower',
         technologies: ['React', 'Javascript/TypeScript', 'CSS', 'Vitejs'],
-        description: 'prom app',
+        description: 'A re-creation of Higher or Lower using Spotify API audio features data. Test your ear for various musical aspects with this fun and frustrating game.',
         GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
         DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/spotifyhigherlower.png',
     },
     {
         name: 'NWProm',
-        technologies: ['Python', 'Django', 'Bootstrap5', 'PostgreSQL', 'Google SSO', 
-                        'Javascript/Typescript'],
-        description: 'prom app',
+        technologies: ['Python', 'Django', 'Bootstrap5', 'PostgreSQL', 'Google SSO'],
+        description: 'A prototype website for prom matchmaking. Avoid embarrassment and fear during prom season through anonymous matchmaking. Served 100+ users in 2022.',
         GithubLink: 'https://github.com/EdwardX29/nwprom',
         DemoLink: 'https://nwprom.herokuapp.com/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/nwprom.png',
 
     },
 
     {
         name: 'Bypass Google Docs',
         technologies: ['Python', 'Flask', 'BeautifulSoup', 'Pandas', 'Bootstrap5', 'CSS'],
-        description: 'prom app',
+        description: 'Download view-only Google Docs and Google Sheets with this app. Input the ID of the document to download and receive it in docx or csv files.',
         GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
         DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/bypassgoogle.png',
     },
 
     {
         name: 'Biographify',
-        technologies: ['Python', 'Django', 'Bootstrap5', 'Spotify API', 
-                        'Spotify SSO', 'CSS', 'html2canvas'],
-        description: 'prom app',
+        technologies: ['Python', 'Django', 'Bootstrap5', 
+                        'Spotify SSO', 'html2canvas'],
+        description: 'Visualize your top Spotify tracks as a biography for the last month, 6 months, and all time. Download your aesthetics to share with your friends.',
         GithubLink: 'https://github.com/EdwardX29/biographify',
         DemoLink: 'https://biographify.herokuapp.com/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/bio.png',
     },
 
     {
         name: 'AP Award Maker',
         technologies: ['Javascript/Typescript', 'React', 'Vitejs', 'CSS', 'Framer'],
-        description: 'prom app',
+        description: 'Create your own Advanced Placement (AP) Awards to show to friends and family. Be sure to send your teachers and professors their own copies.',
         GithubLink: 'https://github.com/EdwardX29/ap-award-clone',
         DemoLink: 'https://edwardx29.github.io/ap-award-clone/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/apaward.png',
     },
 
     {
         name: 'Carti Clicker',
         technologies: ['Javascript/Typescript', 'React', 'Vitejs', 'CSS'],
-        description: 'prom app',
+        description: 'Re-live the era of passive, incremental games with this hip remake of Cookie Clicker. Collect Carti Coins as you make it to the top of the music charts. ',
         GithubLink: 'https://github.com/EdwardX29/CartiClicker',
         DemoLink: 'https://edwardx29.github.io/CartiClicker/',
-        ImageLink:'',
+        ImageLink:'src/assets/images/carti.png',
     },
 
 
