3aae50f40275938d8fa45fcbaad018ee19d40869
Edward <edwardxiao29@gmail.com>
2022-08-16 14:34:01 -0400

    edit image links

--- src/assets/data/projectData.ts
+++ src/assets/data/projectData.ts
@@ -7,7 +7,7 @@ const projects = [
                         Assembled automated school dashboard with Tableau. Served 300+ weekly newsletter subscribers over 1 year.`,
         GithubLink: '',
         DemoLink: 'https://mococovid.com',
-        ImageLink:'src/assets/images/moco.png',
+        ImageLink:'/EdPortfolio/src/assets/images/moco.png',
     },
 
     {
@@ -17,7 +17,7 @@ const projects = [
         music in the 2000s & 2010s through data science. Analysed ~9000 songs spanning two decades of American pop music.`,
         GithubLink: 'https://github.com/EdwardX29/MusicMeta_DataAnalysis',
         DemoLink: 'https://drive.google.com/file/d/1O3GDfK4pVOHF6eLAF9Fg8hFujwIiqEBQ/view',
-        ImageLink:'src/assets/images/musicmeta.jpg',
+        ImageLink:'/EdPortfolio/src/assets/images/musicmeta.jpg',
     },
 
     {
@@ -29,7 +29,7 @@ const projects = [
         hyperbolic) confessions. `,
         GithubLink: 'https://github.com/EdwardX29/umd-confessions',
         DemoLink: 'https://umd-confessions.herokuapp.com/',
-        ImageLink:'src/assets/images/umdconfessions.png',
+        ImageLink:'/EdPortfolio/src/assets/images/umdconfessions.png',
     },
     {
         name: 'Spotify Higher or Lower',
@@ -37,7 +37,7 @@ const projects = [
         description: 'A re-creation of Higher or Lower using Spotify API audio features data. Test your ear for various musical aspects with this fun and frustrating game.',
         GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
         DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
-        ImageLink:'src/assets/images/spotifyhigherlower.png',
+        ImageLink:'/EdPortfolio/src/assets/images/spotifyhigherlower.png',
     },
     {
         name: 'NWProm',
@@ -45,7 +45,7 @@ const projects = [
         description: 'A prototype website for prom matchmaking. Avoid embarrassment and fear during prom season through anonymous matchmaking. Served 100+ users in 2022.',
         GithubLink: 'https://github.com/EdwardX29/nwprom',
         DemoLink: 'https://nwprom.herokuapp.com/',
-        ImageLink:'src/assets/images/nwprom.png',
+        ImageLink:'/EdPortfolio/src/assets/images/nwprom.png',
 
     },
 
@@ -55,7 +55,7 @@ const projects = [
         description: 'Download view-only Google Docs and Google Sheets with this app. Input the ID of the document to download and receive it in docx or csv files.',
         GithubLink: 'https://github.com/EdwardX29/spotify-higher-lower',
         DemoLink: 'https://edwardx29.github.io/spotify-higher-lower/',
-        ImageLink:'src/assets/images/bypassgoogle.png',
+        ImageLink:'/EdPortfolio/src/assets/images/bypassgoogle.png',
     },
     // {
     //     name: "Automated YouTube Channel",
@@ -73,7 +73,7 @@ const projects = [
         description: 'Visualize your top Spotify tracks as a biography for the last month, 6 months, and all time. Download your aesthetics to share with your friends.',
         GithubLink: 'https://github.com/EdwardX29/biographify',
         DemoLink: 'https://biographify.herokuapp.com/',
-        ImageLink:'src/assets/images/bio.png',
+        ImageLink:'/EdPortfolio/src/assets/images/bio.png',
     },
 
 
@@ -84,7 +84,7 @@ const projects = [
         description: 'Create your own Advanced Placement (AP) Awards to show to friends and family. Be sure to send your teachers and professors their own copies.',
         GithubLink: 'https://github.com/EdwardX29/ap-award-clone',
         DemoLink: 'https://edwardx29.github.io/ap-award-clone/',
-        ImageLink:'src/assets/images/apaward.png',
+        ImageLink:'/EdPortfolio/src/assets/images/apaward.png',
     },
 
     {
@@ -93,7 +93,7 @@ const projects = [
         description: 'Re-live the era of passive, incremental games with this hip remake of Cookie Clicker. Collect Carti Coins as you make it to the top of the music charts. ',
         GithubLink: 'https://github.com/EdwardX29/CartiClicker',
         DemoLink: 'https://edwardx29.github.io/CartiClicker/',
-        ImageLink:'src/assets/images/carti.png',
+        ImageLink:'/EdPortfolio/src/assets/images/carti.png',
     },
 
     // {
